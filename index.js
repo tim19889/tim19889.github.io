839aba30d1f7bd5015a40fd624787df916e2f3e8
Tim <101752971+tim19889@users.noreply.github.com>
2023-04-13 14:19:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+let slideShowStatus = 1;
+let imgDisplay = 0;
+let interval;
+let pauseImg = document.getElementById("pause");
+let playImg = document.getElementById("play");
+let welcome1 = document.getElementById("welcome1");
+let welcome2 = document.getElementById("welcome2");
+
+const startSlideShow = () => {
+  interval = setInterval(function() {
+    if (imgDisplay === 0) {
+    welcome1.style.display = "none";
+    welcome2.style.display = "block";
+      imgDisplay = 1;
+    }
+    else {
+      welcome2.style.display = "none";
+    welcome1.style.display = "block";
+      imgDisplay = 0;
+    }
+    }, 6000)
+}
+const startStopSlideShow = () => {
+  if (slideShowStatus === 1) {
+  clearInterval(interval);
+  invterval = undefined;
+    slideShowStatus = 0;
+    playImg.style.display = "none";
+    playImg.style.opacity = 1;
+    pauseImg.style.display = "block";
+    setTimeout(() => {
+  pauseImg.style.opacity = 0;
+}, 1000);
+  }
+  else {
+    startSlideShow();
+    slideShowStatus = 1;
+    pauseImg.style.display = "none";
+    pauseImg.style.opacity = 1;
+    playImg.style.display = "block";
+    setTimeout(() => {
+  playImg.style.opacity = 0;
+}, 1000);
+  }
+}
